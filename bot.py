e9352a3947ffb96ad7a6b359e74c439786f2d747
Andrew <126857875+wasdos@users.noreply.github.com>
2023-03-04 14:36:22 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print('Logged in as {0.user}'.format(client))
-
+#реагування
 @client.event
 async def on_message(message):
     if message.author == client.user:
